2e880ff1013e251ccc5d5103df6d954782eb69b7
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-06-14 15:01:02 +0300

    Update config

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,8 @@
 module.exports = {
   googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123'
+    "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
+  googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
+  mongoURI:
+    "mongodb+srv://Andrey:1a2n3d4r5e6y@graphql-book-app-q0wb4.mongodb.net/test?retryWrites=true&w=majority",
+  cookieKey: "123123123"
 };
