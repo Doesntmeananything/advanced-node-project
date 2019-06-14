e941bbdb7570f820f846b1a5d86f40508103c686
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-06-14 15:07:37 +0300

    Update config

--- config/dev.js
+++ config/dev.js
@@ -3,6 +3,6 @@ module.exports = {
     "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
   googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
   mongoURI:
-    "mongodb+srv://Andrey:1a2n3d4r5e6y@graphql-book-app-q0wb4.mongodb.net/test?retryWrites=true&w=majority",
+    "mongodb+srv://Andrey:goodpassword@graphql-book-app-q0wb4.mongodb.net/test?retryWrites=true&w=majority",
   cookieKey: "123123123"
 };
