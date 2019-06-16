6829dd1cf290e63468db48bc54ac94443a5226b6
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-06-16 16:44:04 +0300

    Add caching to get blogs request

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
+const cleanCache = require("../middlewares/cleanCache");
 
 const Blog = mongoose.model("Blog");
 
@@ -14,33 +15,14 @@ module.exports = app => {
   });
 
   app.get("/api/blogs", requireLogin, async (req, res) => {
-    const redis = require("redis");
-    const redisUrl = "redis://127.0.0.1";
-    const client = redis.createClient(redisUrl);
+    const blogs = await Blog.find({ _user: req.user.id }).cache({
+      key: req.user.id
+    });
 
-    // Make redis commands return promsies
-    const util = require("util");
-    client.get = util.promisify(client.get);
-
-    // Any cached data related to the query?
-    const cachedBlogs = await client.get(req.user.id);
-
-    // if yes, respond immediately and return
-    if (cachedBlogs) {
-      console.log("Serving from cache...");
-      return res.send(JSON.parse(cachedBlogs));
-    }
-
-    // if no, respond to request and update cache
-    const blogs = await Blog.find({ _user: req.user.id });
-
-    console.log("Serving from database...");
     res.send(blogs);
-
-    client.set(req.user.id, JSON.stringify(blogs));
   });
 
-  app.post("/api/blogs", requireLogin, async (req, res) => {
+  app.post("/api/blogs", requireLogin, cleanCache, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
